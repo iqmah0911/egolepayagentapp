fdbff1c8cd42a18790597e126fb9138689115d86
jhayklaus <jamiumoyosore02@gmail.com>
2023-07-10 17:09:57 +0100

    ...

--- src/components/base_components/Footer.js
+++ src/components/base_components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='lg:px-[80px] px-[30px] py-5 bg-white'>
-      <p className='text-sm text-[#7E7E7E]'>© 2023 Biyaego, made with ❤️ by EgolePay . All Rights Reserved.</p>
+      <p className='lg:text-left text-center lg:text-sm text-xs text-[#7E7E7E]'>© 2023 Biyaego, made with ❤️ by EgolePay . All Rights Reserved.</p>
     </div>
   )
 }
