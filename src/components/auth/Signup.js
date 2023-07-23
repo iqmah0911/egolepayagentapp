54db5cd33b6d71fe6809a891f8ca7a6b8453a625
jhayklaus <jamiumoyosore02@gmail.com>
2023-07-23 01:01:15 +0100

    auth screens added

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,24 +1,63 @@
-import React from 'react'
-import BaseInput from '../base_components/BaseInput'
-import Button from '../base_components/Button'
+import React from "react";
+import BaseInput from "../base_components/BaseInput";
+import Button from "../base_components/Button";
 
 const Signup = () => {
+  let formFields = [
+    {
+      label: "Full name",
+      id: "fullname",
+      type: "text",
+      imageStyle: "hidden",
+    },
+
+    {
+      label: "Email",
+      id: "email",
+      type: "email",
+      imageStyle: "hidden",
+    },
+
+    {
+      label: "Password",
+      id: "password",
+      type: "password",
+      imageStyle: "hidden",
+    },
+
+    {
+      label: "Confirm Password",
+      id: "confirm_password",
+      type: "password",
+      imageStyle: "hidden",
+    },
+  ];
+
   return (
-    <div className='h-[100vh] flex flex-col items-center justify-center w-full'>
-      <h1 className='font-bold text-2xl mb-5'>
-        Sign up to continue
-      </h1>
-      <div className='bg-white rounded-md shadow-md w-[50%] py-[50px]'>
-        <form className='w-full flex flex-col items-center'>
-            <BaseInput className={"w-[60%]"} imageStyle={"hidden"} type={"text"} label={"Full name"} id={"fullname"} />
-            <BaseInput className={"w-[60%]"} imageStyle={"hidden"} type={"email"} label={"Email"} id={"email"} />
-            <BaseInput className={"w-[60%]"} imageStyle={"hidden"} type={"password"} label={"Password"} id={"password"} />
-            <BaseInput className={"w-[60%]"} imageStyle={"hidden"} type={"password"} label={"Confirm Password"} id={"confirm_password"} />
-            <Button text={"Proceed"} style={"text-md w-[60%] mt-5 font-bold text-white"} />
+    <div className="h-[100vh] flex flex-col items-center justify-center w-full">
+      <h1 className="font-bold text-2xl mb-5">Sign up to continue</h1>
+      <div className="bg-white rounded-md shadow-md w-[50%] py-[50px]">
+        <form className="w-full flex flex-col items-center">
+          {formFields.map((fields, index) => {
+            return (
+              <BaseInput
+                key={index}
+                className={"w-[60%]"}
+                imageStyle={fields.imageStyle}
+                type={fields.type}
+                label={fields.label}
+                id={fields.id}
+              />
+            );
+          })}
+          <Button
+            text={"Proceed"}
+            style={"text-md w-[60%] mt-5 font-bold p-3 text-white"}
+          />
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
